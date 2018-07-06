bec114ecfbe8f75778736342181ee2480eeb634f
SajadGarshasbi <sajadgarshasbi@icloud.com>
2018-07-07 00:51:30 +0430

    implementing hekmat kard registery

--- app/src/main/java/ir/etkastores/app/activities/profileActivities/hekmatCard/HekmatCardRegisterActivity.java
+++ app/src/main/java/ir/etkastores/app/activities/profileActivities/hekmatCard/HekmatCardRegisterActivity.java
@@ -3,7 +3,6 @@ package ir.etkastores.app.activities.profileActivities.hekmatCard;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.method.PasswordTransformationMethod;
 import android.widget.EditText;
 
@@ -77,4 +76,5 @@ public class HekmatCardRegisterActivity extends BaseActivity implements EtkaTool
     public void onActionClick(int actionCode) {
 
     }
+
 }
