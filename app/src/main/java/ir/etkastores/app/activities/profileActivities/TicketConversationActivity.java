f59cbb9a5d168e72fdc8dd20ce98e32aa1d9c94e
SajadGarshasbi <sajadgarshasbi@icloud.com>
2018-08-20 20:34:13 +0430

    done support ticket

--- app/src/main/java/ir/etkastores/app/activities/profileActivities/TicketConversationActivity.java
+++ app/src/main/java/ir/etkastores/app/activities/profileActivities/TicketConversationActivity.java
@@ -1,12 +1,13 @@
 package ir.etkastores.app.activities.profileActivities;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -23,8 +24,12 @@ import ir.etkastores.app.adapters.recyclerViewAdapters.TicketConversationAdapter
 import ir.etkastores.app.models.OauthResponse;
 import ir.etkastores.app.models.tickets.TicketItem;
 import ir.etkastores.app.models.tickets.TicketRequestModel;
+import ir.etkastores.app.ui.Toaster;
+import ir.etkastores.app.ui.dialogs.MessageDialog;
 import ir.etkastores.app.ui.dialogs.TicketResponseDialog;
 import ir.etkastores.app.ui.views.EtkaToolbar;
+import ir.etkastores.app.ui.views.MessageView;
+import ir.etkastores.app.utils.DialogHelper;
 import ir.etkastores.app.webServices.ApiProvider;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -60,12 +65,19 @@ public class TicketConversationActivity extends BaseActivity implements EtkaTool
     @BindView(R.id.requestDate)
     TextView requestDate;
 
+    @BindView(R.id.messageView)
+    MessageView messageView;
+
+    @BindView(R.id.circularProgress)
+    ProgressBar circularProgress;
+
     private TicketConversationAdapter adapter;
 
     private TicketItem ticketItem;
     private String type;
 
     private TicketRequestModel reqModel;
+    private AlertDialog loadingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,6 +109,12 @@ public class TicketConversationActivity extends BaseActivity implements EtkaTool
     private void initSupportList() {
         toolbar.setTitle(String.format(getResources().getString(R.string.requestCodeX), ticketItem.getId()));
         title.setText(ticketItem.getTitle());
+        loadSupportTickets();
+    }
+
+    private void loadSupportTickets() {
+        messageView.hide();
+        circularProgress.setVisibility(View.VISIBLE);
         Call<OauthResponse<List<TicketItem>>> req = ApiProvider.getAuthorizedApi().getConversation(ticketItem.getTicketCode());
         req.enqueue(new Callback<OauthResponse<List<TicketItem>>>() {
             @Override
@@ -104,17 +122,28 @@ public class TicketConversationActivity extends BaseActivity implements EtkaTool
                 if (response.isSuccessful()) {
                     if (response.body().isSuccessful()) {
                         adapter.addItems(response.body().getData());
+                        if (adapter.getItemCount() == 0) {
+                            messageView.show(R.drawable.ic_warning_orange_48dp, getResources().getString(R.string.yourTicketsListIsEmpty), null, null);
+                        }
                     } else {
-
+                        messageView.show(R.drawable.ic_warning_orange_48dp, response.body().getMeta().getMessage(), null, null);
                     }
                 } else {
                     onFailure(call, null);
                 }
+                circularProgress.setVisibility(View.GONE);
             }
 
             @Override
             public void onFailure(Call<OauthResponse<List<TicketItem>>> call, Throwable t) {
                 if (isFinishing()) return;
+                messageView.show(R.drawable.ic_warning_orange_48dp, R.string.errorInReceivingTicketsListData, R.string.retry, new MessageView.OnMessageViewButtonClick() {
+                    @Override
+                    public void onMessageViewButtonClick() {
+                        loadSupportTickets();
+                    }
+                });
+                circularProgress.setVisibility(View.GONE);
             }
         });
     }
@@ -145,19 +174,56 @@ public class TicketConversationActivity extends BaseActivity implements EtkaTool
         });
     }
 
-    private void sendReply(){
+    private void sendReply() {
+        loadingDialog = DialogHelper.showLoading(this, R.string.inSendingData);
         ApiProvider.getAuthorizedApi().sendSupportTicket(reqModel).enqueue(new Callback<OauthResponse<Long>>() {
             @Override
             public void onResponse(Call<OauthResponse<Long>> call, Response<OauthResponse<Long>> response) {
                 if (isFinishing()) return;
-                if (response.isSuccessful()){
-
+                if (response.isSuccessful()) {
+                    if (response.body().isSuccessful()) {
+                        Toaster.show(TicketConversationActivity.this, R.string.ticketSendSuccessfully);
+                        finish();
+                    } else {
+                        onReplyError(response.body().getMeta().getMessage(), false);
+                    }
+                    loadingDialog.cancel();
+                } else {
+                    onFailure(call, null);
                 }
             }
 
             @Override
             public void onFailure(Call<OauthResponse<Long>> call, Throwable t) {
                 if (isFinishing()) return;
+                onReplyError(getResources().getString(R.string.errorInSendingRequest), true);
+                loadingDialog.cancel();
+            }
+        });
+    }
+
+    public void onReplyError(String message, boolean hasRetry) {
+        String retry = getResources().getString(R.string.retry);
+        if (!hasRetry) retry = null;
+        final MessageDialog messageDialog = MessageDialog.newInstance(R.drawable.ic_warning_orange_48dp,
+                getResources().getString(R.string.error),
+                message,
+                retry,
+                getResources().getString(R.string.close));
+
+        messageDialog.show(getSupportFragmentManager(), false, new MessageDialog.MessageDialogCallbacks() {
+            @Override
+            public void onDialogMessageButtonsClick(int button) {
+                if (isFinishing()) return;
+                if (button == RIGHT_BUTTON) {
+                    sendReply();
+                }
+                messageDialog.getDialog().cancel();
+            }
+
+            @Override
+            public void onDialogMessageDismiss() {
+
             }
         });
     }
